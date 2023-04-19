8f57a8b368f975fa29a5928a1fde8219d56425ad
Reggie188 <mr.tulyakov92@gmail.com>
2023-04-19 15:50:30 +0300

    update task 5 HW first lesson

--- src/less_1/task_5.java
+++ src/less_1/task_5.java
@@ -10,13 +10,50 @@ import java.util.*;
 public class task_5 {
     public static void main(String[] args) {
         List<String> newList = new LinkedList<String>();
-        String a = new String("2? + ?5 = 69");
+        String a = new String("2? + ?2 = 69");
         for (String retval : a.split(" ", 5)) {
             newList.add(retval);
         }
-        List<String> resList = new LinkedList<String>();
+        checkSum(newList);
+    }
 
-        for (int i = 1; i <= 9; i++) {
+    static int Numb(String number, String resExpress, int i) {
+        int cntQuest = count(number, "?");
+        int nxtNum = 0;
+        if (cntQuest == 1) {
+            nxtNum = Integer.valueOf(number.replace("?", (String.valueOf(i))));
+        }
+        return nxtNum;
+    }
+
+    static int count(String str, String target) {
+        return (str.length() - str.replace(target, "").length()) / target.length();
+    }
+
+    static void checkSum(List<String> expression) {
+        int checkRes = 0;
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++){
+                if (Numb(expression.get(0), expression.get(4), i) + Numb(expression.get(2), expression.get(4), j)
+                        == Integer.valueOf(expression.get(4))) {
+                    num1 = Numb(expression.get(0), expression.get(4), i);
+                    num2 = Numb(expression.get(2), expression.get(4), j);
+                    checkRes = num1 + num2;
+                }
+            }
+        }
+        if (checkRes != 0) {
+            System.out.println(num1 + " + " + num2 + " = " + checkRes);
+        } else {
+            System.out.println("Not decisions!");
+        }
+    }
+}
+
+/*
+for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
                 if (Integer.valueOf(newList.get(0).replace("?", (String.valueOf(i)))) +
                         Integer.valueOf(newList.get(2).replace("?", (String.valueOf(j))))
@@ -24,42 +61,6 @@ public class task_5 {
                     System.out.println(newList.get(0).replace("?", (String.valueOf(i))) +
                             " + " + newList.get(2).replace("?", (String.valueOf(i))) + " = "
                             + newList.get(4));
-                    ;
+                    break;
                 }
-            }
-        }
-    }
-}
-/*    static int Numb(String number, String resExpress) {
-        int cntQuest = count(number, "?");
-        if (cntQuest == 1) {
-            for (int i = 0; i <= 9; i++) {
-                return Integer.valueOf(number.replace("?", (String.valueOf(i))));
-            }
-        } else {
-            for (int i = 0; i < Integer.valueOf(resExpress); i++) {
-                return i;
-            }
-        }
-        return 1;
-    }
-
-    static int count(String str, String target) {
-        return (str.length() - str.replace(target, "").length()) / target.length();
-    }
-
-    static int checkSum(List<String> expression) {
-        int checkRes = 0;
-        while (true) {
-            int num1 = Numb(expression.get(0), expression.get(4));
-            int num2 = Numb(expression.get(2), expression.get(4));
-            checkRes = num1 + num2;
-            if (num1 + num2 == Integer.valueOf(expression.get(4))) {
-                break;
-            }
-            System.out.println(num1);
-        }
-        return checkRes;
-    }
-}
 */
