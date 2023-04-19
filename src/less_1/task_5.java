4ebd14e35b0d486ee42cadd4861ae5a1614d83cf
Reggie188 <mr.tulyakov92@gmail.com>
2023-04-19 12:45:06 +0300

    update HW first lesson

--- src/less_1/task_5.java
+++ src/less_1/task_5.java
@@ -4,21 +4,29 @@
 //Предложить хотя бы одно решение или сообщить, что его нет.
 
 package less_1;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 
 public class task_5 {
     public static void main(String[] args) {
-        List<String> newList = new LinkedList<>();
-        String a = new String("2? + ?5 = 69");
-        System.out.println("Возвращаемое значение: ");
+        List<String> newList = new LinkedList<String>();
+        String a = new String("?3 + 4? = 89");
         for (String retval : a.split(" ", 5)) {
             newList.add(retval);
         }
+        System.out.println(newList.get(0));
+
         for (int i = 1; i <= 9; i++) {
-            for (int j = 1; i <= 9; j++) {
-                if ()
+            for (int j = 1; j <= 9; j++) {
+                if (Integer.valueOf(newList.get(0).replace("?", (String.valueOf(i)))) +
+                        Integer.valueOf(newList.get(2).replace("?", (String.valueOf(j))))
+                        == Integer.valueOf(newList.get(4))) {
+                    System.out.println(newList.get(0).replace("?", (String.valueOf(i))) +
+                            " + " + newList.get(2).replace("?", (String.valueOf(i))) + " = "
+                            + newList.get(4));
+                }
             }
         }
     }
 }
+
