923ec4858b4dc6c105427a05bac60c707ff220e4
Reggie188 <mr.tulyakov92@gmail.com>
2023-04-25 17:47:08 +0300

    update HW 4 lesson

--- src/less_3/task_4.java
+++ src/less_3/task_4.java
@@ -2,52 +2,74 @@
 
 package less_3;
 
-import java.util.Arrays;
-
 public class task_4 {
-    public static void main(String[] args) {
-
-        int[] arr = new int[] {3, 5, 7, 8, 4, 2, 1, 5};
-        mergeSort(arr, arr.length);
-        System.out.println(Arrays.toString(arr));
+    public static void main(String[] params) {
+        int[] array = new int[] {15, 78, 89, 9, 12, 12546, 8, 879, 16, 54, 1};
+        System.out.println(arrayToString(array));
+        array = mergeSort(array);
+        System.out.println(arrayToString(array));
     }
-    public static void mergeSort(int[] a, int n) {
-        if (n < 2) {
+
+    public static int[] mergeSort(int[] array) {
+        int[] tmp;
+        int[] currentSrc = array;
+        int[] currentDest = new int[array.length];
+
+        int size = 1;
+        while (size < array.length) {
+            for (int i = 0; i < array.length; i += 2 * size) {
+                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
+            }
+
+            tmp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = tmp;
+
+            size = size * 2;
+
+            //System.out.println(arrayToString(currentSrc));
+        }
+        return currentSrc;
+    }
+
+    private static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int[] dest,
+                              int destStart, int size) {
+        int index1 = src1Start;
+        int index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src1.length);
+        int src2End = Math.min(src2Start + size, src2.length);
+
+        if (src1Start + size > src1.length) {
+            for (int i = src1Start; i < src1End; i++) {
+                dest[i] = src1[i];
+            }
             return;
         }
-        int mid = n / 2;
-        int[] l = new int[mid];
-        int[] r = new int[n - mid];
 
-        for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
-        }
-        for (int i = mid; i < n; i++) {
-            r[i - mid] = a[i];
-        }
-        mergeSort(l, mid);
-        mergeSort(r, n - mid);
+        int iterationCount = src1End - src1Start + src2End - src2Start;
 
-        merge(a, l, r, mid, n - mid);
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
+                dest[i] = src1[index1];
+                index1++;
+            } else {
+                dest[i] = src2[index2];
+                index2++;
+            }
+        }
     }
 
-    public static void merge(
-            int[] a, int[] l, int[] r, int left, int right) {
-
-        int i = 0, j = 0, k = 0;
-        while (i < left && j < right) {
-            if (l[i] <= r[j]) {
-                a[k++] = l[i++];
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
             }
-            else {
-                a[k++] = r[j++];
-            }
+            sb.append(array[i]);
         }
-        while (i < left) {
-            a[k++] = l[i++];
-        }
-        while (j < right) {
-            a[k++] = r[j++];
-        }
+        sb.append("]");
+        return sb.toString();
     }
 }
